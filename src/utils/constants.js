68eef4ded18efadf13822a9b2298af5856faec01
Sam <sambyoung@gmail.com>
2019-11-06 04:01:44 -0500

    rmeoved link from .env, stopped working, made a constant and imported instead

---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const TOKEN_LOGO_404 =
+  'https://icon-icons.com/icons2/1385/PNG/32/generic-crypto-cryptocurrency-cryptocurrencies-cash-money-bank-payment_95340.png';
--- src/utils/imgUtils.js
+++ src/utils/imgUtils.js
@@ -1,4 +1,4 @@
-import { TOKEN_LOGO_404 } from 'dotenv';
+import { TOKEN_LOGO_404 } from './constants';
 
 export const logo404src = e => {
   e.target.src = TOKEN_LOGO_404;
