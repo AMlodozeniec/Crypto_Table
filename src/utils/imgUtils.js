9c6c7a25dc016efc9ffb43720ca6ce06e13edc4f
Sam <sambyoung@gmail.com>
2019-11-06 03:38:15 -0500

    added .dotenv and used it to store long-ass 404 img url

--- src/utils/imgUtils.js
+++ src/utils/imgUtils.js
@@ -1,4 +1,5 @@
+import { TOKEN_LOGO_404 } from 'dotenv';
+
 export const logo404src = e => {
-  e.target.src =
-    'https://icon-icons.com/icons2/1385/PNG/32/generic-crypto-cryptocurrency-cryptocurrencies-cash-money-bank-payment_95340.png';
+  e.target.src = TOKEN_LOGO_404;
 };
